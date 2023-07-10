3d417a78a0ac8b6bba0a2f384acdddfe1c7464dc
Anntia Olivacce <72052149+aolivacce@users.noreply.github.com>
2023-07-10 18:58:21 -0400

    Update fullSQLquery.sql

--- Case Study #5 - Data Mart/fullSQLquery.sql
+++ Case Study #5 - Data Mart/fullSQLquery.sql
@@ -1,9 +1,80 @@
+DROP TABLE IF EXISTS clean_weekly_sales;
+CREATE TEMP TABLE clean_weekly_sales AS (
+SELECT
+  TO_DATE(week_date, 'DD/MM/YY') AS week_date,
+  DATE_PART('week', TO_DATE(week_date, 'DD/MM/YY')) AS week_number,
+  DATE_PART('month', TO_DATE(week_date, 'DD/MM/YY')) AS month_number,
+  DATE_PART('year', TO_DATE(week_date, 'DD/MM/YY')) AS calendar_year,
+  region, 
+  platform, 
+  segment,
+  CASE 
+    WHEN RIGHT(segment,1) = '1' THEN 'Young Adults'
+    WHEN RIGHT(segment,1) = '2' THEN 'Middle Aged'
+    WHEN RIGHT(segment,1) in ('3','4') THEN 'Retirees'
+    ELSE 'unknown' END AS age_band,
+  CASE 
+    WHEN LEFT(segment,1) = 'C' THEN 'Couples'
+    WHEN LEFT(segment,1) = 'F' THEN 'Families'
+    ELSE 'unknown' END AS demographic,
+  transactions,
+  
+  ROUND((sales::NUMERIC/transactions),2) AS avg_transaction,
+  sales
+FROM data_mart.weekly_sales
+);
+
+SELECT * 
+FROM clean_weekly_sales
+LIMIT 20;
+
 -- 1. What day of the week is used for each week_date value?
+
+SELECT DISTINCT(TO_CHAR(week_date, 'day')) AS week_day 
+FROM clean_weekly_sales;
+
 -- 2. What range of week numbers are missing from the dataset?
+
+WITH week_number_cte AS (
+  SELECT GENERATE_SERIES(1,52) AS week_number
+)
+  
+SELECT DISTINCT week_num.week_number
+FROM week_number_cte AS week_num
+LEFT JOIN clean_weekly_sales AS sales
+  ON week_num.week_number = sales.week_number
+WHERE sales.week_number IS NULL;
+
 -- 3. How many total transactions were there for each year in the dataset?
+
+SELECT 
+  calendar_year, 
+  SUM(transactions) AS total_transactions
+FROM clean_weekly_sales
+GROUP BY calendar_year
+ORDER BY calendar_year;
+
 -- 4. What is the total sales for each region for each month?
--- 5. What is the total count of transactions for each platform
+
+SELECT 
+  month_number, 
+  region, 
+  SUM(sales) AS total_sales
+FROM clean_weekly_sales
+GROUP BY month_number, region
+ORDER BY month_number, region;
+
+-- 5. What is the total count of transactions for each platform?
+
+SELECT 
+  platform, 
+  SUM(transactions) AS total_transactions
+FROM clean_weekly_sales
+GROUP BY platform;
+
 -- 6. What is the percentage of sales for Retail vs Shopify for each month?
+
+
 -- 7. What is the percentage of sales by demographic for each year in the dataset?
 -- 8. Which age_band and demographic values contribute the most to Retail sales?
 -- 9. Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
