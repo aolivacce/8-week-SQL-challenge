1faa1f9fae503ae334b73a5a88f8984af30fc72c
Anntia Olivacce <72052149+aolivacce@users.noreply.github.com>
2023-07-11 13:04:58 -0400

    Update fullSQLquery.sql

--- Case Study #5 - Data Mart/fullSQLquery.sql
+++ Case Study #5 - Data Mart/fullSQLquery.sql
@@ -74,7 +74,85 @@ GROUP BY platform;
 
 -- 6. What is the percentage of sales for Retail vs Shopify for each month?
 
+WITH monthly_transactions AS (
+  SELECT 
+    calendar_year, 
+    month_number, 
+    platform, 
+    SUM(sales) AS monthly_sales
+  FROM clean_weekly_sales
+  GROUP BY calendar_year, month_number, platform
+)
+
+SELECT 
+  calendar_year, 
+  month_number, 
+  ROUND(100 * MAX 
+    (CASE 
+      WHEN platform = 'Retail' THEN monthly_sales ELSE NULL END) 
+    / SUM(monthly_sales),2) AS retail_percentage,
+  ROUND(100 * MAX 
+    (CASE 
+      WHEN platform = 'Shopify' THEN monthly_sales ELSE NULL END)
+    / SUM(monthly_sales),2) AS shopify_percentage
+FROM monthly_transactions
+GROUP BY calendar_year, month_number
+ORDER BY calendar_year, month_number;
 
 -- 7. What is the percentage of sales by demographic for each year in the dataset?
+
+WITH demographic_sales AS (
+  SELECT 
+    calendar_year, 
+    demographic, 
+    SUM(sales) AS yearly_sales
+  FROM clean_weekly_sales
+  GROUP BY calendar_year, demographic
+)
+
+SELECT 
+  calendar_year, 
+  ROUND(100 * MAX 
+    (CASE 
+      WHEN demographic = 'Couples' THEN yearly_sales ELSE NULL END)
+    / SUM(yearly_sales),2) AS couples_percentage,
+  ROUND(100 * MAX 
+    (CASE 
+      WHEN demographic = 'Families' THEN yearly_sales ELSE NULL END)
+    / SUM(yearly_sales),2) AS families_percentage,
+  ROUND(100 * MAX 
+    (CASE 
+      WHEN demographic = 'unknown' THEN yearly_sales ELSE NULL END)
+    / SUM(yearly_sales),2) AS unknown_percentage
+FROM demographic_sales
+GROUP BY calendar_year;
+
 -- 8. Which age_band and demographic values contribute the most to Retail sales?
+
+WITH sales_summary AS (
+  SELECT 
+    age_band, 
+    demographic, 
+    SUM(sales) AS retail_sales
+  FROM clean_weekly_sales
+  WHERE platform = 'Retail'
+  GROUP BY age_band, demographic
+)
+SELECT 
+  age_band, 
+  demographic, 
+  retail_sales,
+  ROUND(100 * retail_sales::NUMERIC / SUM(retail_sales) OVER (), 1) AS contribution_percentage
+FROM sales_summary
+ORDER BY retail_sales DESC;
+
 -- 9. Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
+
+SELECT 
+  calendar_year, 
+  platform, 
+  ROUND(AVG(avg_transaction),0) AS avg_transaction_row, 
+  SUM(sales) / sum(transactions) AS avg_transaction_group
+FROM clean_weekly_sales
+GROUP BY calendar_year, platform
+ORDER BY calendar_year, platform;
